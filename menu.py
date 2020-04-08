8008c02f12e54b8710ed0cbe1e8575caca322ec5
Mitch Drake <you@example.com>
2020-04-08 15:52:49 +1000

    Created functional menu system

--- menu.py
+++ menu.py
@@ -1,20 +1,30 @@
 # Faux launcher for options
-def taskSelect():
-    print("Hi There! Please choose an option;")
-    print("1. Create a Ticket = 'c'")
-    print("2. Update a Ticket = 'u'")
-    print("3. Log work for Ticket = 'l'")
-    print("4. Read Existing Ticket = 'r'")
-    print("5. Close a Ticket = 'x'")
-    
-    responce = input()
-"""    
-    if responce = "c":
-        print("Excellent, you'd like to create a new ticket")
-        confirm = input("Please Confirm (y/n)")
-        if confirm = :
-"""
+def printMenu():
+    print(67 * "-")
+    print("Mitch's Ticketing System")
+    print("")
+    print("1. Create a new ticket")
+    print("2. Update an existing ticket")
+    print("3. Read existing ticket")
+    print("4. Close an existing ticket")
+    print("5. Log work for open ticket")
+    print(67 * "-")
+loop=True
 
-confirm = ''
+while loop:
+    printMenu()
+    choice = input ("Enter your choise [1-5]")
 
-taskSelect()
+    if choice==1:
+        print("Create a new ticket")
+    elif choice==2:
+        print("Update a ticket")
+    elif choice==3:
+        print("Read a ticket")
+    elif choice==4:
+        print("Close a ticket")
+    elif choice==5:
+        print("Log work for ticket")
+        loop=False
+    else:
+        raw_input("Wrong option selected. Enter any key to try again...")
